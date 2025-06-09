f88573011a1b94ae18da1e21d5b5d8f82efceed7
6127613876-1 <gokulpriyan781@gmail.com>
2025-06-09 20:00:53 +0530

    9

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -162,49 +162,7 @@ export const Projects = () => {
               </div>
             </div>
 
-<<<<<<< HEAD
             
-=======
-            <div
-              className="
-              glass p-6 rounded-xl border border-white/10 
-              hover:-translate-y-1 hover:border-pink-500/30
-              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
-              transition-all
-            "
-            >
-              <h3 className="text-xl font-bold mb-2">Fake news Detection</h3>
-              <p className="text-gray-400 mb-4">
-              Developed a fake news detection system using web scraping, machine learning, and 
-              automated testing for accurate verification.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Python", "Tensorflow", "Reddit Api"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="
-                      bg-pink-500/10 text-pink-500 py-1 px-3 
-                      rounded-full text-sm
-                      transition
-                      hover:bg-pink-500/20 hover:-translate-y-0.5
-                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center ">
-                <a
-                  href="https://github.com/6127613876-1/fakenews"
-                  target="blank"
-                  className="text-pink-400 hover:text-pink-300 transition-colors my-4"
-                >
-                  View Project →
-                </a>
-              </div>
-            </div>
->>>>>>> 97b8027fb9e883adb363e377064d37f54fe63a37
           </div>
         </div>
       </RevealOnScroll>
